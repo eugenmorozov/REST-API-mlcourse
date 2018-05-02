95ba7104580e40d02ec085ae48be6c70bb0c9440
eugenmorozov <eugen.letterbox@mail.ru>
2018-05-02 17:11:50 +0300

    trying to make it wormking

---
+++ src/main/java/project/Task.java
@@ -0,0 +1,31 @@
+package project;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Task {
+
+    private final Long id;
+    private final String description;
+    private final Boolean completed;
+
+    @JsonCreator
+    public Task(@JsonProperty("id") Long id, @JsonProperty("description") String description, @JsonProperty("completed") Boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+}
