2c1bd35c27ca08c210a2f62758473d7260667f96
eugenmorozov <eugen.letterbox@mail.ru>
2018-06-06 17:45:32 +0300

    fixed indexes

--- src/main/resources/db/migration/V006__init.sql
+++ src/main/resources/db/migration/V006__init.sql
@@ -1,4 +1,4 @@
         CREATE INDEX users_helpful_idx ON users ((lower(nickname)));
-        CREATE INDEX posts_helpful_idx ON posts (id);
+        CREATE INDEX  posts_id_idx ON posts (id);
         CREATE INDEX  posts_helpful_idx ON posts(thread, path);
 
