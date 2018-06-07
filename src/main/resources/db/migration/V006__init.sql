c9d66296d8b49594823480378bbe69d17df3ee6e
eugenmorozov <eugen.letterbox@mail.ru>
2018-06-07 14:47:48 +0300

    moar optimization

--- src/main/resources/db/migration/V006__init.sql
+++ src/main/resources/db/migration/V006__init.sql
@@ -1,4 +1,6 @@
         CREATE INDEX users_helpful_idx ON users ((lower(nickname)));
         CREATE INDEX  posts_id_idx ON posts (id);
         CREATE INDEX  posts_helpful_idx ON posts(thread, path);
+        CREATE INDEX  users_forums_helpful_idx ON forum_users(forum, nickname);
+        CREATE INDEX  threads_search_idx ON threads(forum, created);
 
