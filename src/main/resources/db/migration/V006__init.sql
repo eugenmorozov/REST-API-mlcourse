13be127cd567a7297d6e11eb335ff83a7260ff14
eugenmorozov <eugen.letterbox@mail.ru>
2018-06-07 15:04:06 +0300

    moar optimization

--- src/main/resources/db/migration/V006__init.sql
+++ src/main/resources/db/migration/V006__init.sql
@@ -1,6 +1,9 @@
-        CREATE INDEX users_helpful_idx ON users ((lower(nickname)));
-        CREATE INDEX  posts_id_idx ON posts (id);
-        CREATE INDEX  posts_helpful_idx ON posts(thread, path);
-        CREATE INDEX  users_forums_helpful_idx ON forum_users(forum, nickname);
-        CREATE INDEX  threads_search_idx ON threads(forum, created);
+CREATE TABLE IF NOT EXISTS forum_users (
+        about TEXT DEFAULT NULL,
+        fullname TEXT DEFAULT NULL,
+        nickname CITEXT COLLATE "ucs_basic" REFERENCES  "users"(nickname) ON DELETE CASCADE,
+        email CITEXT,
+        forum CITEXT COLLATE "ucs_basic" REFERENCES "forums"(slug) ON DELETE CASCADE
+);
+
 
